a108efa83afdd5e3b06037b98ce88661d2d60086
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-08-31 11:26:46 -0500

    Added Increment and Decrement

--- java.js
+++ java.js
@@ -3,4 +3,28 @@ $(document).ready(function () {
   var break_length = 5;
 
   $("#session").text(session_length);
+  $("#break").text(break_length);
+
+  $(".session_up").click(function () {
+    session_length += 1;
+    $("#session").text(session_length);
+  });
+
+  $(".session_down").click(function () {
+    session_length -= 1;
+    $("#session").text(session_length);
+  });
+
+  $(".break_up").click(function () {
+    break_length += 1;
+    $("#break").text(break_length);
+  });
+
+  $(".break_down").click(function () {
+    break_length -= 1;
+    $("#break").text(break_length);
+  });
+
+
+
 });
