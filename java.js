56ed555e1d9f500b5a8e2f2428721ed9d21717ed
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-05 10:52:43 -0500

    it counts now

--- java.js
+++ java.js
@@ -1,30 +1,37 @@
 $(document).ready(function () {
   var session_length = 25;
   var break_length = 5;
+  displayTimeSettings();
 
-  $("#session").text(session_length);
-  $("#break").text(break_length);
-
-  $(".session_up").click(function () {
-    session_length += 1;
+  function displayTimeSettings() {
     $("#session").text(session_length);
-  });
-
-  $(".session_down").click(function () {
-    session_length -= 1;
-    $("#session").text(session_length);
-  });
-
-  $(".break_up").click(function () {
-    break_length += 1;
     $("#break").text(break_length);
+  }
+
+  function upDown(value, button) {
+    if (button == "session_button") {
+      session_length += value;
+    } else {
+      break_length += value;
+    }
+    displayTimeSettings();
+  }
+
+  function notZero(button) {
+    if (button == "session_button") {
+      return session_length
+    } else {
+      return break_length
+    }
+  }
+
+  $(".counters").click(function () {
+    var button = $(this).prop("class").split(" ")[1];
+    if ($(this).hasClass("up")) {
+      upDown(1, button);
+    } else if ($(this).hasClass("down") && notZero(button) > 1) {
+      upDown(-1, button);
+    }
   });
-
-  $(".break_down").click(function () {
-    break_length -= 1;
-    $("#break").text(break_length);
-  });
-
-
-
 });
+
